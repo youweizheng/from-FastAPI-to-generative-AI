6f8aee3e3f3db7db326564cea821028c3cb577b6
Youwei Zheng <figoyouwei@gmail.com>
2025-05-20 15:55:47 +0800

    add explore_using_sdk.py

---
+++ app_genai/search/explore_using_sdk.py
@@ -0,0 +1,10 @@
+# app_genai/search_using_sdk.py
+
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+from timescale_vector import client as timescale_client
+from app_genai.database import DATABASE_VECTOR_URL
+
+load_dotenv()
+
